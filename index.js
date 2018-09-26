0fd15ba505a4690c729d8362ff496f840ad2a9e7
alex <apiary23@gmail.com>
2018-09-26 09:36:19 -0500

    enabled install as global script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 'use strict';
 
-const fs = require('fs'),
+const fs = require('fs-extra'),
     path = require('path'),
     pug = require('pug'),
     chokidar = require('chokidar'),
@@ -8,15 +10,16 @@ const fs = require('fs'),
         alias: {
             s: 'srcDir',
             d: 'destDir',
-            o: 'overwrite'
+            p: 'pretty'
         },
         default: {
             srcDir: null,
             destDir: null,
-            overwrite: false
+            pretty: true
         }
     });
 
+// Logging
 const log = function (str) {
     console.log(`\x1b[36m[autopug]\x1b[0m ${str}`);
 };
@@ -24,35 +27,44 @@ const warn = function (str) {
     console.log(`\x1b[93m[autopug]\x1b[0m ${str}`);
 };
 
-let src = args.srcDir,
-    dest = args.destDir;
+// shorthand
+let srcDir = args.srcDir,
+    destDir = args.destDir;
 
-if (!src) {
+if (!srcDir) { // default to ./autopug for source and ./ for destination
     const list = fs.readdirSync('./');
     if (!list.some(n => n === 'autopug')) {
         warn('./autopug not found; creating');
         fs.mkdir('autopug');
     }
-    src = './autopug';
+    srcDir = './autopug';
 }
-if (!dest) {
-    dest = './';
+if (!destDir) {
+    destDir = './';
 }
 
-const normalize = path.sep === '\\'
+// normalize between posix/win32 paths
+const normalize = path.sep !== '\\'
     ? path.normalize.bind(path)
     : path.win32.normalize.bind(path.win32);
-const pugFiles = path.posix.normalize(`${src}**/*.pug`);
-src = normalize(src);
-dest = normalize(dest);
+const pugFiles = path.posix.normalize(`${srcDir}**/*.pug`); // chokidar expects a posix-y glob
+srcDir = normalize(srcDir);
+destDir = normalize(destDir);
 
-warn(pugFiles);
-
-log(`${src} Pug -> ${dest} HTML`);
+log(`Watching: ${srcDir} Pug -> ${destDir} HTML`);
 
 const watcher = chokidar.watch(pugFiles);
-watcher.on('change', (path, stats) => {
-    log(`path: ${path}`);
-    const outputDest = path.replace(src, '').replace('pug', 'html');
-    log(`output to: ${dest}/${outputDest}`);
+watcher.on('change', (filePath) => {
+    const outputDest = filePath.replace(srcDir, '').replace('pug', 'html');
+    let html;
+    try {
+        html = pug.renderFile(filePath, { pretty: args.pretty });
+    } catch (e) { // invalid Pug
+        warn(`WARNING: file ${filePath} contains invalid pug; not compiling`);
+        warn(`${e.message}`);
+        return;
+    }
+    const outName = `${path.join(destDir, outputDest)}`;
+    fs.outputFile(outName, html, { encoding: 'utf-8' });
+    log(`output to: ${outName}`);
 });
